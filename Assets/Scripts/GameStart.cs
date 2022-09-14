f0bc186ac295bf24cd13d9140258549fa355374f
CHARA\meich <mei.charaa@gmail.com>
2022-09-15 00:57:36 +0900

    クレジット関係を追加

--- Assets/Scripts/GameStart.cs
+++ Assets/Scripts/GameStart.cs
@@ -1,47 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class GameStart : MonoBehaviour
 {
     [SerializeField, Header("タイトル")] GameObject _title;
     [SerializeField, Header("スタート")] GameObject _start;
-    Animator _titleAnim;
-    Animator _startAnim;
+    Image _titleImage;
+    Text _startText;
     [SerializeField, Header("ストーリーのText")] GameObject _storyText;
     [SerializeField, Header("フェードした後の間隔")] float _fade;
     [SerializeField, Tooltip("押されたか")] bool _isPushed;
 
     private void Start()
     {
-        _titleAnim = _title.GetComponent<Animator>();
-        _startAnim = _start.GetComponent<Animator>();
+        _titleImage = _title.GetComponent<Image>();
+        _startText = _start.GetComponent<Text>();
         _storyText.SetActive(false);
         _isPushed = false;
+        _startText.DOFade(0.1f, 1.0f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo).SetAutoKill();
+        _start.transform.DOMoveY(-6.0f, 1.0f).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo).SetRelative().SetAutoKill();
     }
 
     void Update()
     {
         //なにかが押されたらシーン移行
-        if(Input.anyKeyDown && !_isPushed)
+        if(Input.GetKeyDown(KeyCode.Return) && !_isPushed)
         {
-            StartCoroutine(Fade());
             _isPushed = !_isPushed;
+            _startText.DORewind();
+            _start.transform.DORewind();
+            StartCoroutine(Fade());
         }
 
     }
 
     IEnumerator Fade()
     {
-        _titleAnim.SetBool("isPressed", true);
-        _startAnim.SetBool("isPressed", true);
+        _titleImage.DOFade(0, _fade).SetEase(Ease.Linear).OnComplete(() => _title.SetActive(false)).SetAutoKill();
+        _startText.DOFade(0, _fade).SetEase(Ease.Linear).OnComplete(() => _start.SetActive(false)).SetAutoKill();
 
         yield return new WaitForSeconds(_fade);
 
-        _title.SetActive(false);
-        _start.SetActive(false);
         _storyText.SetActive(true);
         GameManager.instance._gameMode = GameManager.GameMode.Opening;
     }
--- Assets/Scripts/StoryTextManager.cs
+++ Assets/Scripts/StoryTextManager.cs
@@ -19,6 +19,9 @@ public class StoryTextManager : MonoBehaviour
     bool _goEnd;
     [SerializeField, Tooltip("ゲームオーバー時のストーリー")] TextAsset _gameoverStory;
     string[] _gameover;
+    [SerializeField, Tooltip("クレジットのテキストデータ")] TextAsset _creditText;
+    [Tooltip("上上下下左右左右BA")] KeyCode[] _command = { KeyCode.UpArrow, KeyCode.UpArrow, KeyCode.DownArrow, KeyCode.DownArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.LeftArrow, KeyCode.RightArrow, KeyCode.B, KeyCode.A };
+    [Tooltip("コマンドのリスト")] List<KeyCode> _creditCommand;
     [SerializeField, Header("リトライボタン")] GameObject _retryButton;
     bool _goRetry;
 
@@ -26,14 +29,32 @@ public class StoryTextManager : MonoBehaviour
     {
         _retryButton.SetActive(false);
         _storyText = _storyText.GetComponent<Text>();
-        _start = _startStory.text.Split(char.Parse("\n"));
-        _clear = _clearStory.text.Split(char.Parse("\n"));
-        _gameover = _gameoverStory.text.Split(char.Parse("\n"));
+        //_start = _startStory.text.Split(char.Parse("\n"));
+        //_clear = _clearStory.text.Split(char.Parse("\n"));
+        //_gameover = _gameoverStory.text.Split(char.Parse("\n"));
         _storyText.text = "";
+        _creditCommand = _command.ToList();
     }
 
     void Update()
     {
+        //-----クレジット関係-----
+        if (Input.GetKeyDown(_creditCommand[0]))
+        {
+            _creditCommand.Remove(0);
+            //なんか音鳴らしたい
+        }
+        else if(!Input.GetKeyDown(_creditCommand[0]))
+        {
+            _creditCommand = _command.ToList();
+        }
+
+        if(_creditCommand.Count == 0)
+        {
+            //クレジット流す
+        }
+        //-----ここまで-----
+
         //ゲームが始まったらオープニングストーリーを表示
         if (GameManager.instance._gameMode == GameManager.GameMode.Opening)
         {
@@ -51,7 +72,6 @@ public class StoryTextManager : MonoBehaviour
             else
             {
                 //話が終わったらゲームに移行
-                _storyText.text = "";
                 _storyPanel.GetComponent<Image>().DOFade(0, 2.0f).SetEase(Ease.Linear).OnComplete(() => _storyPanel.SetActive(false)).SetAutoKill();
                 GameManager.instance._gameMode = GameManager.GameMode.PlayGame;
             }
@@ -97,17 +117,23 @@ public class StoryTextManager : MonoBehaviour
     IEnumerator ShowStartStory()
     {
 
-        foreach (var s in _start)
-        {
+        //foreach (var s in _start)
+        //{
 
-            if (s != "空白")
-            {
-                yield return _storyText.DOText($"{_storyText.text}{s}", 3.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
-                _storyText.text = $"{_storyText.text}\n";
-            }
+        //    if (s != "空白")
+        //    {
+        //        yield return _storyText.DOText($"{_storyText.text}{s}\n", 0.5f * (s.Length + _storyText.text.Length)).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        //    }
+        //    else
+        //    {
+        //        yield return _storyText.DOText($"", 2.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        //    }
 
-        }
+        //}
 
+        yield return _storyText.DOText($"{_startStory.text}", 0.2f * _startStory.text.Length).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        yield return new WaitForSeconds(1.0f);
+        _storyText.text = "";
         _endStartStory = true;
     }
 
@@ -117,17 +143,22 @@ public class StoryTextManager : MonoBehaviour
         _storyPanel.SetActive(true);
         yield return _storyPanel.GetComponent<Image>().DOFade(1, 2.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
 
-        foreach (var s in _clear)
-        {
+        //foreach (var s in _clear)
+        //{
 
-            if (s != "空白")
-            {
-                _storyText.text = $"{_storyText.text}\n";
-                yield return _storyText.DOText($"{_storyText.text}{s}", 3.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
-            }
+        //    if (s != "空白")
+        //    {
+        //        yield return _storyText.DOText($"\n{_storyText.text}{s}", 3.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        //    }
+        //    else
+        //    {
+        //        yield return _storyText.DOText($"", 2.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        //    }
 
-        }
+        //}
 
+        yield return _storyText.DOText($"{_clearStory.text}", 0.2f * _clearStory.text.Length).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        yield return new WaitForSeconds(1.0f);
         _goRetry = true;
     }
 
@@ -137,17 +168,23 @@ public class StoryTextManager : MonoBehaviour
         _storyPanel.SetActive(true);
         yield return _storyPanel.GetComponent<Image>().DOFade(1, 2.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
 
-        foreach (var s in _gameover)
-        {
+        //foreach (var s in _gameover)
+        //{
 
-            if (s != "空白")
-            {
-                yield return _storyText.DOText($"{_storyText.text}{s}", 3.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
-                _storyText.text = $"{_storyText.text}\n";
-            }
+        //    if (s != "空白")
+        //    {
+        //        _storyText.text = $"{_storyText.text}\n";
+        //        yield return _storyText.DOText($"{_storyText.text}{s}\n", 0.5f * s.Length).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        //    }
+        //    else
+        //    {
+        //        yield return _storyText.DOText($"", 2.0f).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        //    }
 
-        }
+        //}
 
+        yield return _storyText.DOText($"{_gameoverStory.text}", 0.2f * _gameoverStory.text.Length).SetEase(Ease.Linear).SetAutoKill().WaitForCompletion();
+        yield return new WaitForSeconds(1.0f);
         _goRetry = true;
     }
 }
