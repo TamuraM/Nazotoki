f7819d22df8c4aaa1c7e1d146d74cf456ae5b7fb
CHARA\meich <mei.charaa@gmail.com>
2022-09-24 22:59:18 +0900

    BGMを追加

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField, Header("BGM")] AudioSource[] _sounds;
+    bool _goStart;
+    bool _goCredit;
+    [SerializeField, Header("エンターキー押したときのSE")] AudioClip _start;
+    [SerializeField, Header("エスケープキー押したときのSE")] AudioClip _credit;
+    [SerializeField, Header("リタイアボタン")] RetireGameButton _retire;
+
+    private void Start()
+    {
+        _sounds[0].Play();
+    }
+
+    void Update()
+    {
+        
+        //タイトル画面でエンターキーを押したらSE再生してBGMを止める
+        if(GameManager.instance._gameMode == GameManager.GameMode.Title && Input.GetKeyDown(KeyCode.Return))
+        {
+            _sounds[3].PlayOneShot(_start);
+            _sounds[0].Stop();
+        }
+
+        //タイトル画面でエスケープキーを押したらSE再生してBGMを止める
+        if (GameManager.instance._gameMode == GameManager.GameMode.Title && Input.GetKeyDown(KeyCode.Escape))
+        {
+            _sounds[3].PlayOneShot(_credit);
+            _sounds[0].Stop();
+        }
+
+        //ゲームプレイ画面になったらゲーム時のBGMを流す
+        if (GameManager.instance._gameMode == GameManager.GameMode.PlayGame && !_goStart)
+        {
+            _goStart = true;
+            Debug.Log("ゲームBGM");
+            _sounds[1].Play();
+        }
+
+        //クレジット画面になったらクレジット時のBGMを流す
+        if(GameManager.instance._gameMode == GameManager.GameMode.Credit && !_goCredit)
+        {
+            _goCredit = true;
+            Debug.Log("クレジットBGM");
+            _sounds[2].Play();
+        }
+
+        //ゲームが終わるか、リタイアしたらBGMを止める
+        if(GameManager.instance._gameMode == GameManager.GameMode.GameClear || GameManager.instance._gameMode == GameManager.GameMode.GameOver || _retire.IsClicked)
+        {
+            _sounds[1].Stop();
+        }
+
+    }
+}
--- Assets/Scripts/Button/RetireGameButton.cs
+++ Assets/Scripts/Button/RetireGameButton.cs
@@ -8,10 +8,13 @@ using UnityEngine.UI;
 public class RetireGameButton : ButtonBase
 {
     [SerializeField, Header("ストーリーの背景")] GameObject _storyPanel;
+    bool _isClicked;
+    public bool IsClicked { get => _isClicked; }
 
     /// <summary>クリックされたら、画面がフェードアウトしてタイトルに戻る</summary>
     public override void Click()
     {
+        _isClicked = true;
         _storyPanel.SetActive(true);
         _storyPanel.GetComponent<Image>().DOFade(1, 2.0f).SetEase(Ease.Linear).OnComplete(() => SceneManager.LoadScene("Game")).SetAutoKill();
     }
